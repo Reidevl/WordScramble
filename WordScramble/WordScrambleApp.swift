9f7b28b6e900839b16769ed2ea252d0718c1eec4
Reiwil Lugo <reiwillugo@gmail.com>
2024-02-28 09:13:24 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Reiwil Lugo on 28/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
