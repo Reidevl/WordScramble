96e86315ec6fa4a666292336a6d3ae282c72b895
Reiwil Lugo <reiwillugo@gmail.com>
2024-02-28 11:14:09 -0400

    Add to a list of words

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,39 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        Text("hola")
-    }
-    
-    
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string!
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word.capitalized)
+                        }
+                    }
+                }
             }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
     }
     
-//    func testStrings() {
-//        let input = """
-//        a
-//        b
-//        c
-//        """
-//        let letters = input.components(separatedBy: "\n")
-//        let letter = letters.randomElement()
-//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-//    }
-    
-    func testStrings() {
-        let word = "swift"
-        let checker = UITextChecker()
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        guard answer.count > 0 else { return }
         
-        let allGood = misspelledRange.location == NSNotFound
+        // extra validation to come
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
     }
+
 }
 
 #Preview {
