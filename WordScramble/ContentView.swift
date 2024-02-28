61f86d65a22a0fc5ba88cb516fca508b97544caa
Reiwil Lugo <reiwillugo@gmail.com>
2024-02-28 09:29:16 -0400

    Knowing List component

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(people, id: \.self) {
+            Text("\($0)")
         }
-        .padding()
     }
 }
 
